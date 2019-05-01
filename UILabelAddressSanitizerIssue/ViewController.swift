b029c87a15f6b237ccaf636c60e9ef41f3ad7aab
Maxim Nikolaevich <speakus.net@gmail.com>
2019-05-02 07:25:50 +1000

    Added minimal code to reproduce the issue with Xcode 10.2.1

--- UILabelAddressSanitizerIssue/ViewController.swift
+++ UILabelAddressSanitizerIssue/ViewController.swift
@@ -13,8 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let frame = CGRect.zero // CGRect(x: 27, y: 0, width: 168.5, height: 19.5)
+        let label = UILabel(frame: frame)
+        // changing this line could fix the issue
+        label.text = "****8385"
+        self.runIssue(for: label)
     }
 
-
+    func runIssue(for label: UILabel) {
+        let text = "\u{2022}\u{2022}\u{2022}\u{2022}1234"
+        // let text = "\u{2022}\u{2022}\u{2022}1234" // using one symbol shorter line will fix the issue
+        label.text = text
+        // this line is never reached if AddressSanitizer is enabled
+        print(text)
+    }
 }
 
