bd6e8a07a0cbda6d767f7fca8279956974698e4e
Maxim Nikolaevich <speakus.net@gmail.com>
2019-05-02 07:23:58 +1000

    create empty project from template

---
+++ UILabelAddressSanitizerIssue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UILabelAddressSanitizerIssue
+//
+//  Created by Maxim Nikolaevich on 02/05/2019.
+//  Copyright © 2019 Maxim Nikolaevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
